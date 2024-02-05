3d1fc23de4b31a0645ae82f7cc4e80686f430611
jamesmutune <tunekivasu01@gmail.com>
2024-02-05 12:27:56 +0300

    completed assignments

--- src/components/App.js
+++ src/components/App.js
@@ -23,10 +23,17 @@ function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home/>
+      <About/>
     </div>
   );
+}
+function About(){
+  return(
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  )
 }
 
 export default App;
